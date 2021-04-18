5b705a9b77e95b895eeb2fdaff96a4b782d2db20
Chad Ostrowski <221614+chadoh@users.noreply.github.com>
2021-04-17 20:19:10 -0400

    add loader; functionalize code

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,4 @@
-const defaultMeals = `casserole
+const defaultPossibilities = `casserole
 waffles
 pasta
 tacos
@@ -6,33 +6,46 @@ pizza
 soup
 dal`
 
-let meals = localStorage.getItem('meals')
-if (!meals) {
-  meals = defaultMeals
+const LOCAL_STORAGE_KEY = 'meals'
+
+function getAll ({ raw = false } = {}) {
+  const possibilities = localStorage.getItem(LOCAL_STORAGE_KEY) || defaultPossibilities
+  if (raw) return possibilities
+  return possibilities.split('\n')
 }
 
-const suggestion = document.querySelector('#meal')
+const suggestionEl = document.querySelector('#suggestion')
+if (!suggestionEl) throw new Error('no #suggestion element found; aborting')
 
-if (suggestion) {
-  const array = meals.split('\n')
-  const randomMeal = array[Math.floor(Math.random() * array.length)]
-  suggestion.innerHTML = randomMeal
+async function suggest () {
+  const possibilities = getAll()
+  const suggestion = possibilities[
+    Math.floor(Math.random() * possibilities.length)
+  ]
+  suggestionEl.innerHTML = suggestion
 }
 
-const textarea = document.querySelector('textarea')
-const saveButton = document.querySelector('#save')
-const editButton = document.querySelector('#edit')
-const editArea = document.querySelector('#edit-area')
-if (editButton && editArea && textarea && saveButton) {
-  editButton.addEventListener('click', () => {
-    const editing = editArea.className === 'active'
-    editArea.className = editing ? '' : 'active'
-    if (!editing) textarea.focus()
-  })
+function configureEditor () {
+  const textarea = document.querySelector('textarea')
+  const saveButton = document.querySelector('#save')
+  const editButton = document.querySelector('#edit')
+  const editArea = document.querySelector('#edit-area')
+  if (editButton && editArea && textarea && saveButton) {
+    editButton.addEventListener('click', () => {
+      const editing = editArea.className === 'active'
+      editArea.className = editing ? '' : 'active'
+      if (!editing) textarea.focus()
+    })
 
-  textarea.value = meals
-  saveButton.addEventListener('click', () => {
-    localStorage.setItem('meals', textarea.value)
-    window.location.reload(false)
-  })
+    textarea.value = getAll({ raw: true })
+    saveButton.addEventListener('click', () => {
+      localStorage.setItem(LOCAL_STORAGE_KEY, textarea.value)
+      window.location.reload(false)
+    })
+  }
 }
+
+configureEditor()
+setTimeout(() => suggest(), 750)
+
+
